e950d3c2e8b5bfad958bbfd25801eb2b1173396e
saalim <ksaalim418@gmail.com>
2024-06-11 14:21:15 +0530

    variable

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "key_name" {
+        type = string
+}
+
+variable "public_key" {
+        type = string
+}
+variable "ami" {
+        type = string
+}
+variable "instance_type" {
+        type = string
+}
