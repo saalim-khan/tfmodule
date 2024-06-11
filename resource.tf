2a63369de86ae8712be71171d92b7e8f46c3d35d
saalim <ksaalim418@gmail.com>
2024-06-11 14:33:58 +0530

    rsource part

---
+++ resource.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+module "mywebserver" {
+  source = "./Desktop/modules/webserver"
+  key = file("${path.module}/id_rsa.pub")
+  image_id = "${var.image_id}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+}
+output mypublicIp {
+  value = module.webserver.publicIP
+}
